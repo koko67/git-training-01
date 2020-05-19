e00e8b411893642d6aa40dca1bcb2941ec15e948
Jorge Avila <jorgetop14@gmail.com>
2020-05-19 17:46:38 -0400

    Calculator class completed with all its documentation

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,21 @@
 public class Calculator {
+	// sums 2 numbers
 	public int sum(int n1, int n2) {
+		return n1 + n2;
+	}
+
+	// multiplies 2 numbers
+	public int multiply(int n1, int n2) {
+		return n1 * n2;
+	}
+
+	// rest 2 numbers
+	public int rest(int n1, int n2) {
+		return n1 - n2;
+	}
+
+	// divide 2 numbers
+	public int divide(int n1, int n2) {
+		return n1 / n2;
 	}
 }
